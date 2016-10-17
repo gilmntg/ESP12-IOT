fbe0e372429887d9d81a5c3da9a0ed5fc592d7a2
gilmntg <gilmntg@gmail.com>
2016-10-17 18:53:25 +0300

    * New features:   - Added use of os_timer to time led blinking   - Added new class "Blinker" to blink a led   - Use the Blinker class to signal the state we are in (off,     wifi-connecting, mqtt-connecting)   - Added detection of wifi disconnect and re-connecting accordingly

--- PubSubClient_mqtt_esp8266.ino
+++ PubSubClient_mqtt_esp8266.ino
@@ -2,23 +2,23 @@
 
 
 /*
- Basic ESP8266 MQTT example
+  Basic ESP8266 MQTT example
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
 
- It connects to an MQTT server then:
+  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic" every two seconds
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
   - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
     else switch it off
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
 
- To install the ESP8266 board, (using Arduino 1.6.4+):
+  To install the ESP8266 board, (using Arduino 1.6.4+):
   - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
@@ -32,7 +32,12 @@
 #include <WiFiUdp.h>
 #include <FS.h>
 #include <ArduinoOTA.h>
-#include "Timer.h"
+#include "SimpleTimer.h"
+
+extern "C" {
+#include "user_interface.h"
+}
+
 
 #define HOSTNAME "ESP12E-" ///< Hostename. The setup function adds the Chip ID at the end
 // Update these with values suitable for your network.
@@ -51,8 +56,78 @@ int value = 0;
 
 //GPIO#                            0     1     2     3     4     5     6      7     8       9    10     11    12     13    14    15   16
 bool ESP12E_gpio_exist_list[] = { true, false, true, false, true, true, false, false, false, true, true, false, true, true, true, true, true };
-                                        
+
 String outtopic("");
+
+//SimpleTimer timer;
+
+class Blinker {
+
+  public:
+    enum  BlinkRate { BLINK_RATE_OFF, BLINK_RATE_SLOW, BLINK_RATE_FAST, BLINK_RATE_ON };
+    enum  LedState  { LED_STATE_OFF = 1, LED_STATE_ON = 0 };
+
+  private:
+    static LedState m_led_state;
+    enum Timeout { SLOW_RATE_TIMEOUT = 500, FAST_RATE_TIMEOUT = 100 };
+    static int m_led_gpio;
+//    int m_timer_num;
+    os_timer_t m_timer;
+
+
+  public:
+    Blinker(int gpio_num) {
+      m_led_gpio = gpio_num;
+      m_led_state = LED_STATE_OFF;
+      pinMode(m_led_gpio, OUTPUT);
+      digitalWrite(m_led_gpio, m_led_state);
+    }
+    ~Blinker() {
+      os_timer_disarm(&m_timer);
+      m_led_state = LED_STATE_OFF;
+      digitalWrite(m_led_gpio, m_led_state);
+    };
+    void SetRate(BlinkRate r) {
+      switch (r) {
+        case BLINK_RATE_OFF:
+          os_timer_disarm(&m_timer);
+          m_led_state = LED_STATE_OFF;
+          digitalWrite(m_led_gpio, m_led_state);
+          break;
+        case BLINK_RATE_SLOW:
+          os_timer_disarm(&m_timer);
+          os_timer_setfn(&m_timer, (os_timer_func_t *)ToggleLed, NULL); //pArg = NULL for now
+          os_timer_arm(&m_timer, SLOW_RATE_TIMEOUT,  1); //repetitive
+          break;
+        case BLINK_RATE_FAST:
+            os_timer_disarm(&m_timer);
+            os_timer_setfn(&m_timer, (os_timer_func_t *)ToggleLed, NULL); //pArg = NULL for now
+            os_timer_arm(&m_timer, FAST_RATE_TIMEOUT,  1); //repetitive
+          break;
+        case BLINK_RATE_ON:
+          os_timer_disarm(&m_timer);
+          m_led_state = LED_STATE_ON;
+          digitalWrite(m_led_gpio, m_led_state);
+          break;
+      }
+    }
+
+  private:
+    static void ToggleLed(void *pArg) {
+      if (m_led_state == LED_STATE_OFF) {
+        m_led_state = LED_STATE_ON;
+      } else {
+        m_led_state = LED_STATE_OFF;
+      }
+      digitalWrite(m_led_gpio, m_led_state);
+    }
+};
+
+Blinker::LedState Blinker::m_led_state = Blinker::LED_STATE_OFF;
+int Blinker::m_led_gpio = -1;  //dummy value
+
+Blinker active_led(2); //Indicates connection to Wifi and connection to MQTT status
+
 
 String get_full_hostname() {
   String my_hostname(HOSTNAME);
@@ -65,8 +140,8 @@ String topic_cmd() {
 }
 
 String gpio_list_payload() {
-    String tmp("");
-  for (int i=0; i<sizeof(ESP12E_gpio_exist_list)/sizeof(bool); i++) {
+  String tmp("");
+  for (int i = 0; i < sizeof(ESP12E_gpio_exist_list) / sizeof(bool); i++) {
     if (ESP12E_gpio_exist_list[i]) {
       tmp += String(i, HEX);
       tmp += String(",");
@@ -83,6 +158,7 @@ void setup_wifi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -98,7 +174,7 @@ void setup_wifi() {
 
 String build_payload_string(byte* payload, unsigned int length) {
   String ret("");
-  for (int i=0; i<length; i++) {
+  for (int i = 0; i < length; i++) {
     ret += String((char)payload[i]);
   }
   return ret;
@@ -118,7 +194,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     //parse the  payload
     //payload can be either:
     // "write <<gpio#>> <value>>" OR "read <<gpio#>>" OR "list" OR "out-topic <<some string>>"
-    
+
     char* token = strtok((char*)payload, " \0\n");
     if (String(token) == "out-topic") {
       token = strtok(NULL, " \0\n");
@@ -140,7 +216,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
           Serial.print(gpio_num);
           Serial.print(" The value: ");
           Serial.println(gpio_val);
-          pinMode(gpio_num, OUTPUT); 
+          pinMode(gpio_num, OUTPUT);
           digitalWrite(gpio_num, gpio_val & 0x01);
         } else {
           Serial.println("Invalid gpio_val argument to \"write\" topic");
@@ -154,31 +230,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
         Serial.print("token = ");
         Serial.println(token);
         int gpio_num = String(token).toInt();
-          Serial.print("Received command to read from GPIO# ");
-          Serial.println(gpio_num);
-          pinMode(gpio_num, INPUT); 
-          if (outtopic != "") {
-            client.publish((get_full_hostname() + String("/") + outtopic).c_str(), String(digitalRead(gpio_num)&0x01, HEX).c_str());//TODO
-          }
+        Serial.print("Received command to read from GPIO# ");
+        Serial.println(gpio_num);
+        pinMode(gpio_num, INPUT);
+        if (outtopic != "") {
+          client.publish((get_full_hostname() + String("/") + outtopic).c_str(), String(digitalRead(gpio_num) & 0x01, HEX).c_str()); //TODO
+        }
       } else {
         Serial.println("Invalid gpio_num argument to \"read\" topic");
       }
     } else if (String(token) == "list") {
-        Serial.println("Received command to list all existing GPIOs");
-        if (outtopic != "") {
-          client.publish((get_full_hostname() + String("/") + outtopic).c_str(), gpio_list_payload().c_str());
-        }
+      Serial.println("Received command to list all existing GPIOs");
+      if (outtopic != "") {
+        client.publish((get_full_hostname() + String("/") + outtopic).c_str(), gpio_list_payload().c_str());
+      }
     } else  {
-        Serial.print("unknown command: ");
-        Serial.println((char*)payload);
+      Serial.print("unknown command: ");
+      Serial.println((char*)payload);
     }
   }
 }
 
 
 void reconnect() {
- 
+
   // Loop until we're reconnected
+  active_led.SetRate(Blinker::BLINK_RATE_FAST);
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
@@ -191,13 +268,13 @@ void reconnect() {
       client.publish("iot-devices", get_full_hostname().c_str());
       // ... and resubscribe
       client.subscribe("inTopic", 1);//qos = 1
-      //other client: publish to topic: "FULL-HOSTNAME/cmd", payload: "out-topic <<topic>>" - will set the 
+      //other client: publish to topic: "FULL-HOSTNAME/cmd", payload: "out-topic <<topic>>" - will set the
       //out-topic for all input related commands
-      //Other client: publish to topic: "FULL-HOSTNAME/cmd" payload: "write <<gpio#>> <<val>>" - will configure GPIO# as output 
+      //Other client: publish to topic: "FULL-HOSTNAME/cmd" payload: "write <<gpio#>> <<val>>" - will configure GPIO# as output
       //and write the value
       //other client: publish to topic: "FULL-HOSTNAME/cmd" payload: "read <<gpio#>>" will configure GPIO# to direction=INPUT
       //, read this gpio and publish to topic: "FULL-HOSTNAME/<<out_topic>>" with the payload: <<value>>
-      //other client: publish to topic: "FULL-HOSTNAME/cmd" payload: "list" - will cause this device to publish the gpio list 
+      //other client: publish to topic: "FULL-HOSTNAME/cmd" payload: "list" - will cause this device to publish the gpio list
       //into topic: FULL-HOSTNAME/<<out_topic>> the payload: (e.g.) "0,1,2,4,5" as the existing GPIOs in this device
       client.subscribe(topic_cmd().c_str(), 1);//qos = 1
     } else {
@@ -208,9 +285,13 @@ void reconnect() {
       delay(5000);
     }
   }
+  active_led.SetRate(Blinker::BLINK_RATE_ON);
 }
 
+
 void setup() {
+
+  active_led.SetRate(Blinker::BLINK_RATE_OFF);
 
   Serial.println("\r\n");
   Serial.print("Chip ID: 0x");
@@ -220,13 +301,13 @@ void setup() {
   String hostname(HOSTNAME);
   hostname += String(ESP.getChipId(), HEX);
   WiFi.hostname(hostname);
-  
+
   // Print hostname.
   Serial.println("Hostname: " + hostname);
   //Serial.println(WiFi.hostname());
 
   // Start OTA server.
-    // Start OTA server.
+  // Start OTA server.
   ArduinoOTA.setHostname((const char *)hostname.c_str());
   ArduinoOTA.begin();
 
@@ -236,16 +317,25 @@ void setup() {
   client.setCallback(callback);
   client.setServer(mqtt_server, 1883);
 
+  active_led.SetRate(Blinker::BLINK_RATE_SLOW);
   setup_wifi();
-
+  active_led.SetRate(Blinker::BLINK_RATE_FAST);
 }
 
 void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    active_led.SetRate(Blinker::BLINK_RATE_SLOW);
+    setup_wifi();
+    active_led.SetRate(Blinker::BLINK_RATE_FAST);
+  }
 
   if (!client.connected()) {
     reconnect();
-  }
+  } 
+
   client.loop();
+  //run the led blink timer
+
 #if 0
   long now = millis();
   if (now - lastMsg > 2000) {
@@ -259,6 +349,7 @@ void loop() {
 #endif
   // Handle OTA server.
   ArduinoOTA.handle();
+
   yield();
 
 }
